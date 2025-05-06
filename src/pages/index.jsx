7a9b20b8440c0824cf4f9f824eb8ec05fe5fd212
25LP9262 25LP9262 <25LP9262@admins-iMac.local>
2025-05-05 21:47:51 -0700

    2nd attempt

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import { List } from "@/components/List";
+export const Home = () => {
+  return (
+    <div>
+      <div>
+        <List />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
